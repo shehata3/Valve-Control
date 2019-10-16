5c3d83126347cd3cf21d3f1fb2129fa83cc4d4c0
NPRL <shehata3@illinois.edu>
2019-10-16 10:12:03 -0500

    exec file upd

--- Final Product/Gate Control.bat
+++ Final Product/Gate Control.bat
@@ -1,4 +1,4 @@
-cd C:\Users\CPMI-Fusion1\Desktop\GateControl
+cd C:\valve-control\Final Product
 arduino-cli compile --fqbn arduino:avr:nano ValveControl
 arduino-cli upload -p COM10 --fqbn arduino:avr:nano ValveControl
 start ValveGUI
